c19ca94e363303d1f2d85ac32580b28cf0f2b6ed
Iucha Megreladze <imegreladze.im@gmail.com>
2023-07-16 00:19:22 +0400

    Add project files.

---
+++ ExchangeRateTracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExchangeRateTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
